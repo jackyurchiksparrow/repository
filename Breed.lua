5f652ebc33f0351f66cbf609324a1a4fc5737064
jackyurchiksparrow <39802304+jackyurchiksparrow@users.noreply.github.com>
2020-12-17 22:33:35 +0200

    Update Breed.lua

--- Breed.lua
+++ Breed.lua
@@ -358,7 +358,7 @@ local trash = component.proxy("8d63075a-5549-422e-83d8-b4920383123b") -- interfa
 
 local alv = component.proxy("3ac060b1-a487-491a-b1b3-5bc267d0d5f0") -- interface for alveary
 
-local me = component.proxy("fef863b9-01b7-469a-a6fd-97f32b49c42d") -- interface for me system
+local me = component.proxy("b254c3dd-a91e-477c-9b93-39846fd800fe") -- interface for me system
 
 local analyzer = component.proxy("ab1a1479-19d6-4f9c-9a47-2822cc96d78b") -- analyzer itself
 
